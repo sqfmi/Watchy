5d0822b064a61b59b650a3557551aa87963ef956
SQFMI <github@sqfmi.com>
2021-01-19 00:34:07 -0500

    added MacPaint

---
+++ examples/WatchFaces/MacPaint/Watchy_MacPaint.h
@@ -0,0 +1,13 @@
+#ifndef WATCHY_MACPAINT_H
+#define WATCHY_MACPAINT_H
+
+#include <Watchy.h>
+#include "macpaint.h"
+
+class WatchyMacPaint : public Watchy{
+    public:
+        WatchyMacPaint();
+        void drawWatchFace();
+};
+
+#endif
